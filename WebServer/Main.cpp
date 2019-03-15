28ef2282da189c6983029eb1fda894b318930f22
before25tofree <2869821598@qq.com>
2019-03-15 16:32:07 +0800

    Add argument -t thread_numbers -p port

--- WebServer/Main.cpp
+++ WebServer/Main.cpp
@@ -1,14 +1,39 @@
 #include "EventLoop.h"
 #include "Server.h"
-#include "base/Logging.h"
+#include <getopt.h>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-	#ifdef _PTHREADS
+	int threadNum = 4;
+	int port = 8888;
+	std::string logPath = "./WebServer.log";
+
+	int opt;
+	const char *str = "t:p";
+	while((opt = getopt(argc, argv, str)) != -1)
+	{
+		switch(opt)
+		{
+			case 't':
+			{
+				threadNum = atoi(optarg);
+				break;
+			}
+			case 'p':
+			{
+				port = atoi(optarg);
+				break;
+			}
+			default: break;
+		}
+	}
+
+	#ifndef _PTHREADS
 		//LOG << "_PTHREADS is not defined !";
 	#endif
 	EventLoop mainLoop;
-	Server myHTTPServer(&mainLoop, 4, 8888);
+	Server myHTTPServer(&mainLoop, threadNum, port);
 	myHTTPServer.start();
 	mainLoop.loop();
 	return 0;
